eca4893d06321734ff6d18100329f21938368303
naveengabriel <naveen.gabriel@quantrium-tech.com>
2021-06-22 19:13:22 +0530

    Added a demo code  in get_score to return multiple values

--- cs.py
+++ cs.py
@@ -11,22 +11,32 @@ def import_model():
     model_pkl = open('/Users/priyanshusankhala/Documents/rf.pkl', 'rb')
     model = pickle.load(model_pkl)
     return model
+
 def get_score(v):
     model = import_model()
+    mdl_pred = model.predict(v)
+    mdl_pred_prob = model.perdict_proba(v)
+
+    return [mdl_pred,mdl_pred_prob]
+    
     return model.predict(v)  # To give probablity viz. 1,0
     return model.perdict_proba(v)  # To give probablity value
+
 def import_lg():
     logreg_pkl = open('/Users/priyanshusankhala/Documents/logreg.pkl', 'rb')
     model_lg = pickle.load(logreg_pkl)
     return model_lg
+
 def get_score_lg(v):
     model_lg = import_lg()
     return model_lg.predict(v)  # To give probablity viz. 1,0
     return model_lg.perdict_proba(v)  # To give probablity value
+
 def import_dt():
     dt_pkl = open('http://localhost:8888/files/Documents/descisiontress_model.sav', 'rb')
     model_dt = pickle.load(dt_pkl)
     return model_dt
+
 def get_score_dt(v):
     model_dt = import_dt()
     return model_dt.predict(v)  # To give probablity viz. 1,0
