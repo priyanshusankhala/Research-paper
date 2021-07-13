a37ab863efddd0affa5c62c689499e3f1a3fcb1e
Priyanshu Sankhala <77384596+priyanshusankhala@users.noreply.github.com>
2021-07-13 18:51:32 +0530

    Install first
    commands to execute

---
+++ setup.py
@@ -0,0 +1,13 @@
+# Commands to execute -
+# brew install openssl
+# pip install urllib3
+# pip install simplejson
+# #pip install six, from six.moves.urllib.parse import urlparse
+# pip install os-sys
+# pip install pyenchant
+# pip install numpy
+# pip install requests-html
+# pip install collections-extended
+# pip install beautifulsoup4
+# pip install selenium
+
